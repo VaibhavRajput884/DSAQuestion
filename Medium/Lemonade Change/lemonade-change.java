2479b8cdda6d54cb2cf094a85cc7a38790555280
VaibhavRajput884 <111681149+VaibhavRajput884@users.noreply.github.com>
2023-07-21 20:52:24 +0530

    Updated Solution - GfG to GitHub

--- Medium/Lemonade Change/lemonade-change.java
+++ Medium/Lemonade Change/lemonade-change.java
@@ -28,36 +28,37 @@ class GFG {
 
 
 // User function Template for Java
+
 class Solution {
     static boolean lemonadeChange(int N, int bills[]) {
-        int f = 0;
-        int t = 0;
-        int tw = 0;
-        for(int i:bills){
-            if(i == 5){
-                f+=1;
-            }else if(i == 10){
-                t += 1;
-                if(f>0){
-                    f -=1;
-                }else{
-                    return false;
-                }
-            }else{
-                tw += 1;
-                if(t >0){
-                    t-=1;
-                    if(f>0){
-                        f-=1;
-                    }else{
-                        return false;
+        // code here
+        int five=0;
+        int ten=0;
+        for(int i=0;i<N;i++){
+            int x=bills[i];
+            if(x==5){
+                five++;
+            }
+            else if(x==10){
+                five--;
+                ten++;
+            }
+            else{
+                if(x==20){
+                    if(ten>0){
+                        ten--;
+                        five--;
+                        
                     }
-                }else if(t==0 && f>=3){
-                    f-=3;
-                }else{
-                    return false;
+                    else{
+                        five-=3;
+                    }
                 }
             }
+            if(five<0)
+            return false;
+            
+            
         }
         return true;
     }
