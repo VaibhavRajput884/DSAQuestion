07f41e7a0ead1f0137daf64f67d8c964496a4025
VaibhavRajput884 <111681149+VaibhavRajput884@users.noreply.github.com>
2023-07-23 16:57:15 +0530

    Added solution - LeetHub

---
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..java
@@ -0,0 +1,119 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    public static void main (String[] args) throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n =Integer.parseInt(in.readLine().trim());
+            String s[] = in.readLine().trim().split(" ");
+            Node head = new Node(Integer.parseInt(s[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(s[i]));
+                tail = tail.next;
+            }
+
+            head = new Solution().segregate(head);
+            printList(head, out);
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static void printList(Node head,PrintWriter out)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+    
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    //Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head)
+    {
+        // add your code here
+         HashMap<Integer,Integer> numCount = new HashMap<>();
+        Node temp = head;
+        int count = 0;
+        while(temp != null){
+            if(numCount.containsKey(temp.data)){
+            count = numCount.get(temp.data);
+            numCount.put(temp.data,count + 1);
+            }else{
+                numCount.put(temp.data, 1);
+            }
+            temp = temp.next;
+        }
+        temp = head;
+        int number = 0;
+        for(int i  = 0 ; i < 3 ; i++){
+            if(!numCount.containsKey(i)){
+                continue;
+            }
+            else{
+                number = numCount.get(i);
+                while(number != 0){
+                    temp.data = i;
+                    temp = temp.next;
+                    number--;
+                }
+            }
+       
+        }
+        return head;
+    }
+}
+
+
