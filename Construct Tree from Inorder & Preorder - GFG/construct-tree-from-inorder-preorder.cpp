6adfbbad351a991c710e63d1ad132a9f12731663
VaibhavRajput884 <111681149+VaibhavRajput884@users.noreply.github.com>
2023-07-25 17:02:14 +0530

    Added solution - LeetHub

---
+++ Construct Tree from Inorder & Preorder - GFG/construct-tree-from-inorder-preorder.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void printPostOrder(Node *root)
+{
+	if(root==NULL)
+		return;
+	printPostOrder(root->left);
+	printPostOrder(root->right);
+	cout<<root->data<<" ";
+}
+
+// } Driver Code Ends
+
+/*Complete the code here.
+Node is as follows:
+struct Node
+{
+  int data;
+  Node* left;
+  Node* right;
+};
+*/
+class Solution{
+    public:
+    int index=0;
+    Node * tree (int in[],int pre[],int is,int ie){
+        if(is>ie) return NULL;
+        Node * root=new Node(pre[index++]);
+        int ind;
+        for(int i=is;i<=ie;i++){
+            if(in[i]==root->data){
+                ind=i;
+                break;
+            }
+        }
+        root->left=tree(in,pre,is,ind-1);
+        root->right=tree(in,pre,ind+1,ie);
+        return root;
+        
+    }
+    Node* buildTree(int in[],int pre[], int n)
+    {
+        return tree(in,pre,0,n-1);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		
+		int inorder[n], preorder[n];
+		for(int i=0; i<n; i++)
+			cin>> inorder[i];
+		for(int i=0; i<n; i++)
+			cin>> preorder[i];
+		Solution obj;
+		Node *root = obj.buildTree(inorder, preorder, n);
+		printPostOrder(root);
+		cout<< endl;
+	}
+}
+
+// } Driver Code Ends
