7cf2875376c67565c72cda155287ea4d04902d07
VaibhavRajput884 <111681149+VaibhavRajput884@users.noreply.github.com>
2023-07-27 11:35:29 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Smaller on Left/smaller-on-left.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    int sizeOfArray = Integer.parseInt(br.readLine());
+		    int arr [] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Complete obj = new Complete();
+		    arr = obj.Smallestonleft(arr, sizeOfArray);
+		    for(int i=0; i< sizeOfArray;i++){
+		        System.out.print(arr[i] + " ");
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int[] Smallestonleft (int arr[], int n) {
+        // Complete the function
+        TreeSet<Integer> s= new TreeSet<>();
+        s.add(-1);
+        for(int i=0;i<n;i++){
+            s.add(arr[i]);
+            arr[i]=s.lower(arr[i]);
+            
+        }
+        return arr;
+    }
+    
+    
+}
+
+
