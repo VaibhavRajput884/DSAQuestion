2c6789f671ba4991af16123becf8afbdf95ca428
VaibhavRajput884 <111681149+VaibhavRajput884@users.noreply.github.com>
2023-08-20 00:11:09 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.java
+++ Number of occurrence - GFG/number-of-occurrence.java
@@ -37,13 +37,12 @@ public class Main {
 
 class Solution {
     int count(int[] arr, int n, int x) {
+        // code here
         int k=0;
         for(int i=0;i<n;i++){
             if(arr[i]==x) k++;
             
         }
         return k;
-        
-        // code here
     }
 }
