a4c0d6a1123463cce82ebeba67da6683b5388708
VaibhavRajput884 <111681149+VaibhavRajput884@users.noreply.github.com>
2023-08-22 00:04:13 +0530

    Added solution - LeetHub

---
+++ Make Matrix Beautiful - GFG/make-matrix-beautiful.java
@@ -0,0 +1,124 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntMatrix
+{
+    public static int[][] input(BufferedReader br, int n, int m) throws IOException
+    {
+        int[][] mat = new int[n][];
+
+        for(int i = 0; i < n; i++)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            mat[i] = new int[s.length];
+            for(int j = 0; j < s.length; j++)
+                mat[i][j] = Integer.parseInt(s[j]);
+        }
+
+        return mat;
+    }
+
+    public static void print(int[][] m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+
+    public static void print(ArrayList<ArrayList<Integer>> m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[][] matrix = IntMatrix.input(br, N, N);
+            
+            Solution obj = new Solution();
+            int res = obj.findMinOperation(N, matrix);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+
+
+static int findMinOperation( int n,int arr[][])
+
+    {
+
+        // code here 
+
+        int [] row = new int[n];
+
+        int [] col = new int[n];
+
+    
+
+        for(int i=0;i<n;i++){
+
+            int rsum =0;
+
+            int csum =0;
+
+            for(int j=0;j<n;j++){
+
+                rsum+= arr[i][j];
+
+                csum+= arr[j][i];
+
+            }
+
+            row[i]=rsum;
+
+            col[i] =csum;
+
+        }
+
+        int max =0;
+
+        for(int i=0;i<n;i++){
+
+            max = Math.max(max,Math.max(row[i],col[i]));
+
+        }
+
+        int ans =0;
+
+        for(int i=0;i<n;i++){
+
+            ans += (max-row[i]);
+
+        }
+
+        return ans;
+
+    }}
+        
