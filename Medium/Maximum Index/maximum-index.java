bd47b179514772df624533c97c07c2b3f4e858a8
VaibhavRajput884 <111681149+VaibhavRajput884@users.noreply.github.com>
2023-10-15 11:49:24 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum Index/maximum-index.java
+++ Medium/Maximum Index/maximum-index.java
@@ -2,47 +2,62 @@
 import java.io.*;
 import java.util.*;
 
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t =
-            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            int arr[] = new int[n];
-            String inputLine[] = br.readLine().trim().split(" ");
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-            System.out.println(new Solution().maxIndexDiff(arr, n));
-        }
-    }
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim()); // size of array
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]); // input elements of array
+		    }
+		    
+		    Solution ob = new Solution();
+		    
+		    System.out.println(ob.maxIndexDiff(arr, n)); // print the result
+		}
+	}
 }
 // } Driver Code Ends
 
 
-class Solution {
-
-  int maxIndexDiff(int arr[], int n) {
-        // code here
-        int ans = 0;
-        Stack<Integer> stack = new Stack<>();
-        //loop for storing index in stack whose value appears in decreasing order
-        for(int i = 0; i < n; i++) {
-            if(stack.isEmpty() || arr[stack.peek()] > arr[i]) {
-                stack.push(i);
-            }
-        }
-        //Now we traverse from right to left
-        int i = n-1;
-        while(i>=0) {
-            if(!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
-                ans = Math.max(ans, i-stack.pop());
-                continue;
-            }
-            i--;
-        }
-        return ans;
-    }
+class Solution{
+    
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+   static int maxIndexDiff(int A[], int N) { 
+        
+        // Your code here
+      int left[]=new int[N];
+      int right[]=new int[N];
+       left[0]=A[0];
+      right[N-1]=A[N-1];
+      for(int i=1;i<N;i++){
+          left[i]=Math.min(A[i],left[i-1]);
+      }
+      for(int j=N-2;j>=0;j--)
+      {
+          right[j]=Math.max(right[j+1],A[j]);
+      }
+      int i=0,j=0;
+      int diff=0;
+      while(i<N && j<N){
+          if(left[i]<=right[j]){
+          diff=Math.max(diff,j-i);
+          j++;
+          }
+          else
+          i++;
+      }
+      return diff;
 }
+
+
+
+}
+
+
