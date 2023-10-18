d9276a99c367b705b58d956577fb4e2cea418084
VaibhavRajput884 <111681149+VaibhavRajput884@users.noreply.github.com>
2023-10-18 11:29:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count distinct elements in every window/count-distinct-elements-in-every-window.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.util.HashMap;
+
+class GFG
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0)
+        {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) 
+                a[i] = sc.nextInt();
+            Solution g = new Solution();
+            
+            ArrayList<Integer> ans = g.countDistinct(a, n, k);
+
+            for (Integer val: ans) 
+                System.out.print(val+" "); 
+            System.out.println();
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution
+{
+    ArrayList<Integer> countDistinct(int arr[], int n, int k)
+    {
+        ArrayList<Integer > ans = new ArrayList<>();
+        HashMap<Integer , Integer >  map = new HashMap<>();
+        int i=0 , j=0;
+        while(i<k){
+            map.put(arr[i] , map.getOrDefault(arr[i] , 0)+1);
+            i++;
+        }
+        ans.add(map.size());
+        while(i<n){
+            // acquire
+            map.put(arr[i] , map.getOrDefault(arr[i] , 0)+1);
+            // release
+            int freq = map.get(arr[j]);
+            if(freq ==1){
+                map.remove(arr[j]);
+            }else{
+                map.put(arr[j] , freq-1);
+            }
+            j++;
+            // work
+            ans.add(map.size());
+            i++;
+        }
+        return ans;
+    }
+}
